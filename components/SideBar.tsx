09f6cd9538c04694dd2d3521b623af4f52ad42bd
Ivan Okello <74784829+Ivandamien@users.noreply.github.com>
2023-03-06 17:16:29 +0300

    Buildig the user account

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,22 +1,39 @@
-import React from 'react'
-import NewChat from './NewChat'
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import React from "react";
+import NewChat from "./NewChat";
 
 function SideBar() {
-  return (
-      <div className='p-2 flex-col h-screen'>
-         <div className="flex-1">
-              <div>
-                  {/*Newchat  */}
-                    <NewChat/>
-                  <div>
-                    {/* ModelSelection */}
-                  </div>
+  const { data: session } = useSession();
 
-                  {/* Map through the chatRows */}
-              </div>  
-          </div> 
+  return (
+    <div className="p-2 flex flex-col h-screen">
+      <div className="flex-1">
+        <div>
+          {/*Newchat  */}
+          <NewChat />
+
+          <div>{/* ModelSelection */}</div>
+
+          {/* Map through the chatRows */}
+        </div>
       </div>
-  )
+
+      {session && (
+        <img
+          src={session.user?.image!}
+          alt="profile picture"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+        />
+      )}
+      <button
+        onClick={() => signOut()}
+        className="bg-black text-white px-4 py-2 rounded-lg hover:opacity-50"
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
 
-export default SideBar
+export default SideBar;
